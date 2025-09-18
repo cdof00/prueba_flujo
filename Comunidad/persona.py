bec67e5e34ed8b1661b1c525862f0f79ca1d42f3
cdof00 <cd.ortiz@uniandes.edu.co>
2025-09-17 21:09:46 -0500

    Se agrega xyz (Verde)

--- Comunidad/persona.py
+++ Comunidad/persona.py
@@ -19,6 +19,7 @@ class Persona:
    def dar_nombre(self):
        return(self.__nombre)
    #pruebas
+   #merge auto prueba
 
    def calcular_anio_nacimiento(self, ya_cumplio_anios):
        anio_actual = datetime.datetime.now().year
